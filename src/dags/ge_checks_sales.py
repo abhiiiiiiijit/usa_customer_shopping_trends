958e925f5f2d880f0c509593f71b82a884795880
abhiiiiiiijit <abhijitjan22@gmail.com>
2024-12-16 20:45:30 +0100

    DAG created

--- src/dags/ge_checks_sales.py
+++ src/dags/ge_checks_sales.py
@@ -5,10 +5,12 @@ from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
 from airflow.models.dag import DAG
 from great_expectations_provider.operators.great_expectations import GreatExpectationsOperator
-from great_expectations.data_context.types.base import (
-    DataContextConfig,
-    CheckpointConfig
-)
+# from great_expectations.data_context.types.base import (
+#     DataContextConfig,
+#     CheckpointConfig
+# )
+
+
 
 # Define default arguments for the DAG
 default_args = {
@@ -21,24 +23,24 @@ default_args = {
 }
 
 # Define variables (update paths and configs)
-ge_root_dir = "/path/to/your/great_expectations"  # Replace with your GE root directory
-example_data_context_config = DataContextConfig(  # Replace with your GE data context config
-    datasources={},
-    stores={},
-    expectations_store_name="expectations_store",
-    validations_store_name="validations_store",
-    evaluation_parameter_store_name="evaluation_parameter_store",
-    plugins_directory=None,
-    config_version=2.0,
-    validation_operators={},
-)
-example_checkpoint_config = CheckpointConfig(  # Replace with your GE checkpoint config
-    name="example_checkpoint",
-    config_version=1.0,
-    class_name="Checkpoint",
-    run_name_template=None,
-    validations=[]
-)
+ge_root_dir = "/home/adminabhi/gitrepo/usa_customer_shopping_trends/src/gx"  # Replace with your GE root directory
+# example_data_context_config = DataContextConfig(  # Replace with your GE data context config
+#     datasources={},
+#     stores={},
+#     expectations_store_name="expectations_store",
+#     validations_store_name="validations_store",
+#     evaluation_parameter_store_name="evaluation_parameter_store",
+#     plugins_directory=None,
+#     config_version=2.0,
+#     validation_operators={},
+# )
+# example_checkpoint_config = CheckpointConfig(  # Replace with your GE checkpoint config
+#     name="example_checkpoint",
+#     config_version=1.0,
+#     class_name="Checkpoint",
+#     run_name_template=None,
+#     validations=[]
+# )
 
 # Define the DAG
 with DAG(
@@ -51,16 +53,13 @@ with DAG(
     tags=['ge', 'data quality'],
 ) as dag:
 
-    ge_data_context_root_dir_with_checkpoint_name_pass = GreatExpectationsOperator(
-        task_id="ge_data_context_root_dir_with_checkpoint_name_pass",
-        data_context_root_dir=ge_root_dir,
-        checkpoint_name="taxi.pass.chk",
+    ge_validate_sales_data = GreatExpectationsOperator(
+        task_id = 'ge_validate_sales_data',
+        data_context_root_dir = ge_root_dir,
+        conn_id = 'airflow_to_gcs',
+        expectation_suite_name = 'sales_suite',
+        data_asset_name = 'usa-customer-shopping-trends.sales.shopping_trends',
+        fail_task_on_validation_failure =False,
+        return_json_dict = True,
     )
-
-    ge_data_context_config_with_checkpoint_config_pass = GreatExpectationsOperator(
-        task_id="ge_data_context_config_with_checkpoint_config_pass",
-        data_context_config=example_data_context_config,
-        checkpoint_config=example_checkpoint_config,
-    )
-
-    ge_data_context_root_dir_with_checkpoint_name_pass >> ge_data_context_config_with_checkpoint_config_pass
+    ge_validate_sales_data
